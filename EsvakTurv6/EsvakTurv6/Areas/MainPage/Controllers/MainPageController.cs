b522a940231c2c1b2e15963ab874cc30b15f218d
Yusuf Ekrem İkiz <nebilemben3283@gmail.com>
2026-08-02 18:53:50 +0300

    Kategori Görme
    Kategori görme eklendi ve ana sayfada küçülmeye gidildi.

--- EsvakTurv6/EsvakTurv6/Areas/MainPage/Controllers/MainPageController.cs
+++ EsvakTurv6/EsvakTurv6/Areas/MainPage/Controllers/MainPageController.cs
@@ -8,13 +8,27 @@ using System.Web.Mvc;
 namespace EsvakTurv6.Areas.MainPage.Controllers
 {
     public class MainPageController : Controller
-
     {
         EsvakTurv6.Models.EsvakTur db = new EsvakTurv6.Models.EsvakTur();
-        // GET: MainPage/MainPage
+
+      
         public ActionResult Index()
         {
-            var tours = db.Tours.Where(t => !t.IsDeleted && t.IsActive).ToList();
+
+            var tours = db.Tours.Where(t => !t.IsDeleted && t.IsActive)
+                                .OrderByDescending(t => t.ID)
+                                .ToList();
+
+           
+            ViewBag.FeaturedTours = db.Tours.Where(t => !t.IsDeleted && t.IsActive)
+                                            .OrderBy(t => t.Price) 
+                                            .Take(3)
+                                            .ToList();
+
+           
+            ViewBag.TotalTours = db.Tours.Count(t => !t.IsDeleted && t.IsActive);
+            ViewBag.TotalCategories = db.TourCategories.Count(); 
+           
 
             return View(tours);
         }
@@ -28,5 +42,153 @@ namespace EsvakTurv6.Areas.MainPage.Controllers
             }
             return View(tour);
         }
+        public ActionResult ContactInf()
+        {
+            return View();
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//The soul becomes dyed with the colour of its thoughts.- Marcus Aurelius
---
+++ EsvakTurv6/EsvakTurv6/Areas/MainPage/Controllers/SeeCategoriesController.cs
@@ -0,0 +1,18 @@
+﻿using EsvakTurv6.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace EsvakTurv6.Areas.MainPage.Controllers
+{
+    public class SeeCategoriesController : Controller
+    {
+        private EsvakTur db = new EsvakTur();
+
+        // GET: MainPage/SeeCategories
+        public ActionResult Index()
+        {
+            var categories = db.TourCategories.ToList();
+            return View(categories);
+        }
+    }
+}
