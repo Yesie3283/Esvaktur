48e6b924b8114629151922aafc17e9d9838b2a6b
Yusuf Ekrem İkiz <nebilemben3283@gmail.com>
2026-08-04 07:12:40 +0300

    Ödeme sistemi 2
    Sistemin giriş gereksinimi ve diğer problemleri halledildi.

--- EsvakTurv6/EsvakTurv6/Areas/MainPage/Controllers/MainPageController.cs
+++ EsvakTurv6/EsvakTurv6/Areas/MainPage/Controllers/MainPageController.cs
@@ -11,24 +11,19 @@ namespace EsvakTurv6.Areas.MainPage.Controllers
     {
         EsvakTurv6.Models.EsvakTur db = new EsvakTurv6.Models.EsvakTur();
 
-      
         public ActionResult Index()
         {
-
             var tours = db.Tours.Where(t => !t.IsDeleted && t.IsActive)
                                 .OrderByDescending(t => t.ID)
                                 .ToList();
 
-           
             ViewBag.FeaturedTours = db.Tours.Where(t => !t.IsDeleted && t.IsActive)
-                                            .OrderBy(t => t.Price) 
+                                            .OrderBy(t => t.Price)
                                             .Take(3)
                                             .ToList();
 
-           
             ViewBag.TotalTours = db.Tours.Count(t => !t.IsDeleted && t.IsActive);
-            ViewBag.TotalCategories = db.TourCategories.Count(); 
-           
+            ViewBag.TotalCategories = db.TourCategories.Count();
 
             return View(tours);
         }
@@ -42,6 +37,7 @@ namespace EsvakTurv6.Areas.MainPage.Controllers
             }
             return View(tour);
         }
+
         public ActionResult ContactInf()
         {
             return View();
@@ -49,6 +45,13 @@ namespace EsvakTurv6.Areas.MainPage.Controllers
 
         public ActionResult Checkout(int id)
         {
+            
+            if (Session["UserID"] == null)
+            {
+                TempData["ErrorMessage"] = "Ödeme yapabilmek ve rezervasyon oluşturmak için lütfen önce giriş yapın.";
+                return RedirectToAction("Login", "Register", new { area = "MainPage" });
+            }
+
             var tour = db.Tours.Find(id);
             if (tour == null)
             {
@@ -60,146 +63,4 @@ namespace EsvakTurv6.Areas.MainPage.Controllers
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//The soul becomes dyed with the colour of its thoughts.- Marcus Aurelius
+// "The soul becomes dyed with the colour of its thoughts." - Marcus Aurelius
